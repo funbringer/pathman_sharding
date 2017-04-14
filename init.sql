4f9a533a3d40586cc2aee34888fb97a25203c0d2
Dmitry Ivanov <ivadmi5@gmail.com>
2017-04-14 11:48:51 +0300

    introduce view @extschema@.pathman_foreign_partition_list

--- init.sql
+++ init.sql
@@ -14,3 +14,16 @@ $$
 		END IF;
 	END
 $$;
+
+
+CREATE OR REPLACE VIEW @extschema@.pathman_foreign_partition_list AS
+SELECT ppl.*,
+	   fdw.fdwname AS fdw_name,
+	   fs.srvname AS server,
+	   fs.srvoptions AS server_options
+FROM @extschema@.pathman_partition_list AS ppl
+JOIN pg_catalog.pg_foreign_table AS ft ON ppl.partition = ft.ftrelid
+JOIN pg_catalog.pg_foreign_server AS fs ON ft.ftserver = fs.oid
+JOIN pg_catalog.pg_foreign_data_wrapper AS fdw ON fs.srvfdw = fdw.oid;
+
+GRANT SELECT ON @extschema@.pathman_foreign_partition_list TO PUBLIC;
