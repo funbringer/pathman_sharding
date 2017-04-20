087676738673e3db86a916a3fd211f569a1e2aaa
Dmitry Ivanov <ivadmi5@gmail.com>
2017-04-20 18:02:58 +0300

    introduce function distribute_partitions_among_servers()

--- init.sql
+++ init.sql
@@ -27,3 +27,47 @@ JOIN pg_catalog.pg_foreign_server AS fs ON ft.ftserver = fs.oid
 JOIN pg_catalog.pg_foreign_data_wrapper AS fdw ON fs.srvfdw = fdw.oid;
 
 GRANT SELECT ON @extschema@.pathman_foreign_partition_list TO PUBLIC;
+
+
+/*
+ * Distribute partitions among foreign servers.
+ */
+CREATE OR REPLACE FUNCTION @extschema@.distribute_partitions_among_servers(
+	IN partitions	TEXT[],
+	IN servers		TEXT[],
+	OUT partition	TEXT,
+	OUT server		TEXT)
+RETURNS SETOF RECORD AS $$
+DECLARE
+	current_parts		TEXT[];
+	current_parts_len	INT8;
+	partitions_len		INT8 := array_length(partitions, 1);
+	servers_len			INT8 := array_length(servers, 1);
+	j					INT8;
+
+BEGIN
+	IF array_ndims(partitions) != 1 THEN
+		RAISE EXCEPTION '"partitions" should have exactly 1 dimension';
+	END IF;
+
+	IF array_ndims(servers) != 1 THEN
+		RAISE EXCEPTION '"servers" should have exactly 1 dimension';
+	END IF;
+
+	FOR current_parts IN (SELECT partitions[i: i + servers_len - 1]
+						  FROM generate_series(1, partitions_len, servers_len) i)
+	LOOP
+		current_parts_len := array_length(current_parts, 1);
+
+		FOR j IN 1..current_parts_len
+		LOOP
+			partition := current_parts[j];
+			server := servers[j];
+
+			RETURN NEXT;
+		END LOOP;
+	END LOOP;
+
+	RETURN;
+END
+$$ language plpgsql STRICT;
